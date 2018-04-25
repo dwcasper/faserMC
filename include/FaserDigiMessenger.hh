e1c183a38631cfd2a8a08b7104c89b2eb2401881
swaneyjr <jeffrey.r.swaney@vanderbilt.edu>
2018-04-25 11:20:51 -0700

    Added messenger for digitization and changed ADC to charge

---
+++ include/FaserDigiMessenger.hh
@@ -0,0 +1,31 @@
+#ifndef FaserDigiMessenger_h
+#define FaserDigiMessenger_h
+
+#include "G4UImessenger.hh"
+#include "globals.hh"
+
+class FaserDigitizer;
+class G4UIdirectory;
+class G4UIcommand;
+class G4UIcmdWithADoubleAndUnit;
+
+class FaserDigiMessenger : public G4UImessenger
+{
+  public:
+    FaserDigiMessenger(FaserDigitizer* digitizer);
+    ~FaserDigiMessenger();
+
+  public:
+    virtual void SetNewValue(G4UIcommand* command, G4String newValues);
+    virtual G4String GetCurrentValue(G4UIcommand* command);
+
+  private:
+    FaserDigitizer* fDigitizer;
+
+  private:
+    G4UIdirectory*		digiDirectory;
+    G4UIcmdWithADoubleAndUnit*	cmd_digi_chargeSpreadSigma;
+    G4UIcmdWithADoubleAndUnit*	cmd_digi_threshold;
+};
+
+#endif
