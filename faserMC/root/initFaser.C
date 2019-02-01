6d529f9409f9c5def7bc4498e01cd7c212dde06b
Dave Casper <dcasper@uci.edu>
2019-02-01 07:01:54 -0800

    remove reference to obsolete library

--- faserMC/root/initFaser.C
+++ faserMC/root/initFaser.C
@@ -4,10 +4,8 @@ void initFaser()
   {
     string libDir = string(getenv("FASERMC_DIR")) + "/faserMC_run/lib64/";
     string libFaserRootClasses = libDir + "libFaserRootClassesDict.so";
-    string libFaserTrackerClasses = libDir + "libFaserTrackerClassesDict.so";
 
     gSystem->Load(libFaserRootClasses.c_str());
-    gSystem->Load(libFaserTrackerClasses.c_str());
   }
 }
 
